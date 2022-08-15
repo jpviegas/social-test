fb18f3aa289a87fcf6cdcfc9275cf77b78f1770b
jpviegas <jp.viegas@hotmail.com>
2022-08-15 20:40:05 -0300

    fix: title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+// import Head from "next/head";
+
 export default function Home() {
   // document.title = "home";
   return (
     <main>
+      {/* <Head>
+        <title>home</title>
+      </Head> */}
       <h1>home</h1>
     </main>
   );
